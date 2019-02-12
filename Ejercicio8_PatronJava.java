390e38561d7c9d4c6331a9c454ff767b8acca66e
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-02-11 20:30:39 -0700

    Add files via upload

---
+++ Ejercicio8_PatronJava.java
@@ -0,0 +1,14 @@
+/*Marco Carrera
+11-02-19
+2:15-2:25*/
+
+	class Ejercicio8_PatronJava{
+	public static void main (String[] args){
+
+		System.out.println("    J     a    v     v  a");
+		System.out.println("    J    a a    v   v  a a");
+		System.out.println(" J  J   aaaaa    v v  aaaaa");
+		System.out.println("  JJ   a     a    v  a     a"); 
+		//imprime el patron de la palabra java
+	}
+	}
